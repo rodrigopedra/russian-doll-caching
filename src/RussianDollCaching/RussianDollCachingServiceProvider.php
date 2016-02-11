96a3568ac6e699158315d1cc9a2f417d42a6420a
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2016-02-11 04:47:12 -0200

    fix service provider

--- src/RussianDollCaching/RussianDollCachingServiceProvider.php
+++ src/RussianDollCaching/RussianDollCachingServiceProvider.php
@@ -9,14 +9,14 @@ class RussianDollCachingServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app[ 'blade.compiler' ]->directive( 'russian', function ( $expression ) {
-            return "<?php echo app('\\RodrigoPedra\\RussianDollCaching')->get{$expression}; ?>";
+            return "<?php echo app('\\RodrigoPedra\\RussianDollCaching\\RussianDollCaching')->get{$expression}; ?>";
         } );
     }
 
     public function register()
     {
         $this->app->singleton( RussianDollCaching::class, function ( $app ) {
-            return new RussianDollCaching( $app[ 'cache' ], $app[ 'view' ] );
+            return new RussianDollCaching( $app[ 'cache' ]->store(), $app[ 'view' ] );
         } );
     }
 }
